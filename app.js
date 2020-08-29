e829dc4e5a5ddd08e2400dbc15b4dfa3fc499bab
Justin Huang <justinhuang777@gmail.com>
2020-08-29 23:13:08 +0800

    add main and index page content

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
 
-app.engine('hbs', exphbs({ extname: '.hbs'}))
+app.engine('hbs', exphbs({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
-app.get('/', (req, res) => res.render('home'))
-
+app.get('/', (req, res) => res.render('index'))
 app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`))
